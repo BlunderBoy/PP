73ca7ae0c7bc2ac74d29ff8d59d2c94c40887ea4
Cristian <cristian@Cris-PC.localdomain>
2020-02-24 02:05:35 +0200

    nu inteleg de ce nu merge functia impar

--- lab1/lab.hs
+++ lab1/lab.hs
@@ -1,3 +1,4 @@
+import Data.List
 -- exercitiul 1
 f _ = 5
 -- exercitiul 2
@@ -9,17 +10,43 @@ functieAnd True False = True
 functieAnd False True = True
 functieAnd False False = False
 -- exercitiul 4
+    -- semnatura este g :: t1 -> t -> t1 si f :: Num t => t1 -> t
 -- exercitiul 5
+myIf :: Bool -> a -> a -> a
+myIf True atunci atlfel = atunci
+myIf False atunci atlfel = atlfel
 -- exercitiul 6
+maxim :: Integer -> Integer -> Integer -> Integer
+maxim a b c = max a (max b c)
 -- exercitiul 7
+f1 x y z = if x then y else z
+    --f1 :: Bool -> t -> t -> t
 -- exercitiul 8
+--max cu &&? nah
 -- exercitiul 9
 -- exercitiul 10
+rev :: [Integer] -> [Integer]
+rev l = (revi l [])
+
+revi :: [Integer] -> [Integer] -> [Integer]
+revi [] a = a
+revi a b = (revi (tail a) ( (head a):b) )
 -- exercitiul 11
 -- exercitiul 12
+antepen :: [Integer] -> Bool
+antepen a = if mod (head (tail (rev a))) 2 == 0 then True else False
 -- exercitiul 13
+sumator :: [Integer] -> Integer
+sumator [] = 0
+sumator (x:xs) = x + sumator(xs)
 -- exercitiul 14
+checker :: [Bool] -> Bool
+checker [] = True
+checker (x:xs) = if (x == False) then False else checker xs
 -- exercitiul 15
+impar :: [Integer] -> [Integer]
+impar [] = []
+impar (x:xs) = if (mod x 2) == 0 then (impar xs) else impar (delete x xs)
 -- exercitiul 16
 -- exercitiul 17
 -- exercitiul 18
